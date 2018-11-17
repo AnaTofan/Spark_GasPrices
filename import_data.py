511fad7599009bf100a4cd58b1f5f68378eccd5a
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-11-17 16:24:39 +0100

    Added gitignore in gas_data

--- import_data.py
+++ import_data.py
@@ -1,1 +1,14 @@
-import pyspark
+import pyspark as pyspark
+import os
+
+os.environ['SPARK_HOME'] = '/home/ana/spark/spark-2.3.2-bin-hadoop2.7'
+sc = pyspark.SparkContext.getOrCreate()
+sc.setLogLevel("OFF")
+
+sql_sc = pyspark.SQLContext(sc)
+
+prices2014_df = (sql_sc
+             .read.option('header', 'false')
+             .option('sep', ';')
+             .option('inferSchema', 'true')
+             .csv('gas_data/Prix2014.csv'))
